d574f4ca7b65686cc9f22a1575426f063f271423
swifthorseman <swifthorseman@protonmail.com>
2017-10-08 13:06:15 +0100

    Format source code using scalariform #5

--- project/CiCommands.scala
+++ project/CiCommands.scala
@@ -3,6 +3,6 @@ import sbt.Command
 object CiCommands {
   def ciBuild: Command = Command.command("ciBuild") { state =>
     "clean" :: "coverage" :: "test" :: "coverageReport" :: "coverageAggregate" ::
-    state
+      state
   }
 }
