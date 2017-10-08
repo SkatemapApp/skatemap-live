7edd84761c5a062d6e5777fbab325f715b5443c0
swifthorseman <swifthorseman@protonmail.com>
2017-10-08 14:07:41 +0100

    Add return type to a public function #5

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,7 +1,7 @@
 package controllers
 
 import javax.inject._
-import play.api._
+
 import play.api.mvc._
 
 /**
@@ -18,7 +18,7 @@ class HomeController @Inject() (cc: ControllerComponents) extends AbstractContro
    * will be called when the application receives a `GET` request with
    * a path of `/`.
    */
-  def index() = Action { implicit request: Request[AnyContent] =>
+  def index(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
   }
 }
