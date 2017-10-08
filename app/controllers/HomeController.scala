3358a2166113646701f1abcf82b3622b3a6985d5
swifthorseman <swifthorseman@protonmail.com>
2017-10-08 21:59:35 +0100

    Do not return an HTML page in the response #13

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -19,6 +19,6 @@ class HomeController @Inject() (cc: ControllerComponents) extends AbstractContro
    * a path of `/`.
    */
   def index(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+    Ok
   }
 }
