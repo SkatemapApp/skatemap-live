b77a308241d4081031170ad4806d668dd2f849f2
swifthorseman <swifthorseman@users.noreply.github.com>
2024-08-26 13:25:31 +0100

    Remove existing framework files #18

--- project/CiCommands.scala
+++
@@ -1,9 +0,0 @@
-import sbt.Command
-
-object CiCommands {
-  def ciBuild: Command = Command.command("ciBuild") { state =>
-    "clean" :: "coverage" :: "test" :: "coverageReport" :: "coverageAggregate" ::
-      "scalastyle" :: "test:scalastyle" ::
-      state
-  }
-}
